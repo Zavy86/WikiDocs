ec6c10fc4fb3483bc6718f122ba66bbd956e0c79
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-01-12 14:17:09 +0100

    Setup script

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -17,8 +17,8 @@ define("SUBTITLE","flat-file markdown wiki engine");
 define("OWNER","Manuel Zavatta | Zavynet.org");
 // Contents credits notice
 define("NOTICE","Copyright 2009-".date("Y")." &copy; All rigths reserved.");
-// MD5 encoded password for reading (set null for public wiki)
-define("VIEWCODE","ecae13117d6f0584c25a9da6c8f8415e");
 // MD5 encoded password for editing
 define("EDITCODE","efb2a684e4afb7d55e6147fbe5a332ee");
+// MD5 encoded password for reading (set null for public wiki)
+define("VIEWCODE","ecae13117d6f0584c25a9da6c8f8415e");
 ?>
