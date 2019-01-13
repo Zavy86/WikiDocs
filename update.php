c3f2ed52e77a433d867ab36a4a18999deb1f2216
Manuel Zavatta <manuel.zavatta@gmail.com>
2019-01-13 14:55:09 +0100

    Updater fix

--- update.php
+++ update.php
@@ -12,29 +12,22 @@
  // mode definition
  define(MODE,"engine");
  // check authentication
- if(wdf_authenticated()!=2){
-  // alert and redirect
-  wdf_alert("You are not authenticated!","danger");
-  wdf_redirect(PATH.$p_document);
- }
- // check for localhost
- if(in_array($_SERVER['HTTP_HOST'],array("localhost","127.0.0.1"))){
-  // alert and redirect
-  wdf_alert("Git pull denied on localhost!","danger");
-  wdf_redirect(PATH);
- }
+ if(wdf_authenticated()!=2){die("You are not authenticated!");}
  // check for git
- if(!is_dir(DIR.".git")){
-  // alert and redirect
-  wdf_alert("Git directory not found!","danger");
-  wdf_redirect(PATH);
+ if(is_dir(DIR.".git")){
+  wdf_dump("Git repository found!",null,null,true);
+  // check for localhost
+  if(in_array($_SERVER['HTTP_HOST'],array("localhost","127.0.0.1"))){
+   die("Git pull denied non localhost!");
+  }else{
+   // make command
+   $command="cd ".DIR." ; pwd ; git stash 2>&1 ; git stash clear ; git pull 2>&1 ; chmod 755 -R ./";
+   // exec shell commands
+   $shell_output=exec('whoami')."@".exec('hostname').":".shell_exec($command);
+   // debug
+   wdf_dump($shell_output,"shell_exec",null,true);
+  }
+ }else{
+  die("Git repository not found!");
  }
- // make command
- $command="cd ".DIR." ; pwd ; git stash 2>&1 ; git stash clear ; git pull 2>&1 ; chmod 755 -R ./";
- // debug
- //wdf_dump($command,"command");
- // exec shell commands
- $shell_output=exec('whoami')."@".exec('hostname').":".shell_exec($command);
- // debug
- wdf_dump($shell_output,"shell_exec"); 
 ?>
