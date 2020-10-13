45362e84024e4487db5ed70dfaf7b5dc68d16681
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-10-13 14:46:08 +0200

    ROOT fix

--- functions.inc.php
+++ functions.inc.php
@@ -38,7 +38,8 @@ if(!strlen($g_doc)){$g_doc="homepage";}
 define("DEBUG",$debug);
 define("VERSION",file_get_contents("VERSION.txt"));
 define("HOST",(isset($_SERVER['HTTPS'])?"https":"http")."://".$_SERVER['HTTP_HOST']);
-define("ROOT",str_replace(PATH, "", str_replace("\\","/",realpath(dirname(__FILE__))."/")));
+//define("ROOT",str_replace(PATH, "", str_replace("\\","/",realpath(dirname(__FILE__))."/"))); /* @todo check with reyemxela */
+define("ROOT",rtrim(str_replace("\\","/",realpath(dirname(__FILE__))."/"),PATH));
 define("URL",HOST.PATH);
 define("DIR",ROOT.PATH);
 define("DOC",$g_doc);
