c5506c990694fa7738a6ef71c05623b076585190
Manuel Zavatta <manuel.zavatta@gmail.com>
2024-01-03 09:32:05 +0100

    add timezone to wdf_timestamp_format

--- functions.inc.php
+++ functions.inc.php
@@ -15,13 +15,13 @@
  * @param bool $force Force dump also if debug is disabled
  */
 function wdf_dump($variable,?string $label=null,?string $class=null,bool $force=false):void{
-	if(!DEBUG && !$force){return;}
-	echo "\n<!-- dump -->\n";
-	echo "<pre class='debug ".$class."'>\n";
-	if($label<>null){echo "<b>".$label."</b>\n";}
-	if(is_string($variable)){$variable=str_replace(array("<",">"),array("&lt;","&gt;"),$variable);}
-	print_r($variable);
-	echo "</pre>\n<!-- /dump -->\n";
+  if(!DEBUG && !$force){return;}
+  echo "\n<!-- dump -->\n";
+  echo "<pre class='debug ".$class."'>\n";
+  if($label<>null){echo "<b>".$label."</b>\n";}
+  if(is_string($variable)){$variable=str_replace(array("<",">"),array("&lt;","&gt;"),$variable);}
+  print_r($variable);
+  echo "</pre>\n<!-- /dump -->\n";
 }
 
 /**
@@ -30,8 +30,8 @@ function wdf_dump($variable,?string $label=null,?string $class=null,bool $force=
  * @param string $location Location URL
  */
 function wdf_redirect(string $location):void{
-	if(DEBUG){die("<a href=\"".$location."\">".$location."</a>");}
-	exit(header("location: ".$location));
+  if(DEBUG){die("<a href=\"".$location."\">".$location."</a>");}
+  exit(header("location: ".$location));
 }
 
 /**
@@ -42,30 +42,30 @@ function wdf_redirect(string $location):void{
  * @return bool
  */
 function wdf_alert(string $message,string $class="info"):bool{
-	// checks
-	if(!$message){return false;}
-	// build alert object
-	$alert=new stdClass();
-	$alert->timestamp=time();
-	$alert->message=$message;
-	$alert->class=$class;
-	// check for debug
-	if(!DEBUG){
-		// add alert to session alerts
-		$_SESSION['wikidocs']['alerts'][]=$alert;
-	}else{
-		// swicth class
-		switch($class){
-			case "success":$message="(!) ".$message;break;
-			case "warning":$message="/!\\ ".$message;break;
-			case "danger":$message="<!> ".$message;break;
-			default:$message="(?) ".$message;
-		}
-		// dump alert
-		wdf_dump($message,"ALERT");
-	}
-	// return
-	return true;
+  // checks
+  if(!$message){return false;}
+  // build alert object
+  $alert=new stdClass();
+  $alert->timestamp=time();
+  $alert->message=$message;
+  $alert->class=$class;
+  // check for debug
+  if(!DEBUG){
+    // add alert to session alerts
+    $_SESSION['wikidocs']['alerts'][]=$alert;
+  }else{
+    // swicth class
+    switch($class){
+      case "success":$message="(!) ".$message;break;
+      case "warning":$message="/!\\ ".$message;break;
+      case "danger":$message="<!> ".$message;break;
+      default:$message="(?) ".$message;
+    }
+    // dump alert
+    wdf_dump($message,"ALERT");
+  }
+  // return
+  return true;
 }
 
 /**
@@ -77,21 +77,24 @@ function wdf_alert(string $message,string $class="info"):bool{
  * @throws Exception
  */
 function wdf_timestamp_format(?int $timestamp,string $format="Y-m-d H:i:s"){
-	if(!is_numeric($timestamp) || $timestamp==0){return false;}
-	// build date time object
-	$datetime=new DateTime("@".$timestamp);
-	// return date time formatted
-	return $datetime->format($format);
+  if(!is_numeric($timestamp) || $timestamp==0){return false;}
+  // build date time object
+  $datetime=new DateTime("@".$timestamp);
+  if(defined('TIMEZONE') && TIMEZONE != 'default'){
+    $datetime->setTimezone(new DateTimeZone(TIMEZONE));
+  }
+  // return date time formatted
+  return $datetime->format($format);
 }
 
 /**
  * Regenerate Sitemap
  */
 function wdf_regenerate_sitemap(){
-	$baseURL=URL;
-	$lastMod=date('Y-m-d\TH:i:sP',Document::getUpdateDate("/homepage"));
-	// open sitemap
-	$sitemap=<<<EOS
+  $baseURL=URL;
+  $lastMod=date('Y-m-d\TH:i:sP',Document::getUpdateDate("/homepage"));
+  // open sitemap
+  $sitemap=<<<EOS
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
  <url>
@@ -100,34 +103,34 @@ function wdf_regenerate_sitemap(){
  </url>
 
 EOS;
-	// add documents from root
-	$sitemap.=wdf_regenerate_sitemap_documents();
-	// close sitemap
-	$sitemap.=<<<EOS
+  // add documents from root
+  $sitemap.=wdf_regenerate_sitemap_documents();
+  // close sitemap
+  $sitemap.=<<<EOS
 </urlset>
 
 EOS;
-	// write sitemap to file
-	file_put_contents(DIR."sitemap.xml",$sitemap);
+  // write sitemap to file
+  file_put_contents(DIR."sitemap.xml",$sitemap);
 }
 function wdf_regenerate_sitemap_documents(?string $parent=null){
-	$sitemap='';
-	$documents=Document::index($parent);
-	// cycle all documents
-	foreach($documents as $document){
-		// set variables
-		$url=URL.$document->url;
-		$lastMod=date('Y-m-d\TH:i:sP',Document::getUpdateDate($document->url));
-		// add to sitemap
-		$sitemap.=<<<EOS
+  $sitemap='';
+  $documents=Document::index($parent);
+  // cycle all documents
+  foreach($documents as $document){
+    // set variables
+    $url=URL.$document->url;
+    $lastMod=date('Y-m-d\TH:i:sP',Document::getUpdateDate($document->url));
+    // add to sitemap
+    $sitemap.=<<<EOS
  <url>
   <loc>$url</loc>
   <lastmod>$lastMod</lastmod>
  </url>
 
 EOS;
-		// add sub documents recursively
-		$sitemap.= wdf_regenerate_sitemap_documents($document->url);
-	}
-	return $sitemap;
+    // add sub documents recursively
+    $sitemap.= wdf_regenerate_sitemap_documents($document->url);
+  }
+  return $sitemap;
 }
