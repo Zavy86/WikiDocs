a31d572d352d6d9cddad804d5ec19205470a21c3
shaoziyang <shaoziyang@126.com>
2024-06-27 01:40:30 +0800

    Update common.php

--- common.php
+++ common.php
@@ -1,22 +1,16 @@
 <?php
 
-function console_log($data){
-    if (is_array($data) || is_object($data))
-        echo("<script>console.log(".json_encode($data).");</script>");
-    else
-        echo("<script>console.log('".$data."');</script>");
-}
-
-// random int number per day
+// generate daily random numbers for daily quotes
 function randint_day(){
     $D1 = new DateTime('1900-01-01');
     $D2 = new DateTime();
-    $n = $D1->diff($D2)->days;
-    for($i=0;$i<5;$i++)
+    $n = $D1->diff($D2)->days;  // get date difference
+    for($i=0;$i<5;$i++)         // iteration 5 times
         $n = (142857*$n+31)%65536;
     return $n;
 }
 
+// return image filename array
 function getImagelist($path) {
     $image_list = array_merge(glob($path.'*.png'), glob($path.'*.jpg'), glob($path.'*.jpeg'), glob($path.'*.svg'), glob($path.'*.webp'));
     foreach ($image_list as &$v){
@@ -24,6 +18,5 @@ function getImagelist($path) {
     }
     return $image_list;
 }
-
 
 ?>
