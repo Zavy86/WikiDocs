3572aca417e6431a3f25159441014b944bf47454
Manuel Zavatta <manuel.zavatta@gmail.com>
2026-08-06 09:16:30 +0200

    Refactor ESLint configuration to remove Prettier integration and adjust type annotation spacing rules

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -10,7 +9,6 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
     languageOptions: {
       globals: {
@@ -29,15 +27,6 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
-      '@typescript-eslint/type-annotation-spacing': [
-        'error',
-        {
-          before: false,
-          after: false,
-          overrides: { arrow: { before: true, after: true } },
-        },
-      ],
       '@typescript-eslint/explicit-member-accessibility': [
         'warn',
         {
