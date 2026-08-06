0bc3e7c76c4581507423695859edf7f8b929b808
Manuel Zavatta <manuel.zavatta@gmail.com>
2026-08-06 09:04:59 +0200

    Add ReleaseService for managing latest release checks and updates

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from "@nestjs/core";
 import { JwtModule } from "@nestjs/jwt";
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from "src/app.controller";
 import { AppGuard } from "src/app.guard";
-import { AccountsService, DocumentService, EnvironmentService, PinnedService, SearchService, SettingsService, SystemService, SyncService } from "src/services";
+import { AccountsService, DocumentService, EnvironmentService, PinnedService, ReleaseService, SearchService, SettingsService, SystemService, SyncService } from "src/services";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
     JwtModule
   ],
   controllers: [
@@ -19,6 +21,7 @@ import { AccountsService, DocumentService, EnvironmentService, PinnedService, Se
     DocumentService,
     EnvironmentService,
     PinnedService,
+    ReleaseService,
     SearchService,
     SettingsService,
     SystemService,
